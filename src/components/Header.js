a14a63dbbe7c467771ff9d069f47eb37dfe151cb
casterminator <44626745+casterminator@users.noreply.github.com>
2019-06-19 17:26:13 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,10 @@ import { Link } from 'gatsby';
 import '../assets/sass/main.scss';
 const Header = () => (
   <header id="header">
-    <Link className="title" to="/">
-      DINERO
-    </Link>
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/generic">Generic</Link>
-        </li>
-        <li>
-          <Link to="/elements">Elements</Link>
+          <Link to="/">Back to Home</Link>
         </li>
       </ul>
     </nav>
