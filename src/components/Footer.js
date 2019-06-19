6aa81b830f1f970ea9bb7edc9c43ede65b4d5ef2
casterminator <44626745+casterminator@users.noreply.github.com>
2019-06-19 17:56:43 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,6 @@ const Footer = () => (
       <ul className="menu">
         <li>Dinero Coin Project. All rights reserved 2019.</li>
         <li>
-          Forked from: <a href="https://github.com/anubhavsrivastava/gatsby-starter-hyperspace">Anubhav Srivastava</a>
-        </li>
-        <li>
           <a href="https://github.com/dinerocoin/Dinero-Docs/blob/master/Dinero_Privacy_Policy.pdf">Privacy Policy</a>
         </li>
       </ul>
