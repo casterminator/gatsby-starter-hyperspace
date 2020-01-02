6b8600f2b85e3b74be30dab2a732bd2ccb7820ce
casterminator <44626745+casterminator@users.noreply.github.com>
2020-01-02 18:30:55 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
   <footer id="footer" className="wrapper style1-alt">
     <div className="inner">
       <ul className="menu">
-        <li>Dinero Coin Project. All rights reserved 2019.</li>
+        <li>Dinero Coin Project. All rights reserved 2020.</li>
         <li>
           <a href="https://github.com/dinerocoin/Dinero-Docs/blob/master/Dinero_Privacy_Policy.pdf">Privacy Policy</a>
         </li>
