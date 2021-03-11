84638c2e418daf098e0b7c5928e424bf758e13bc
casterminator <44626745+casterminator@users.noreply.github.com>
2021-03-11 16:50:03 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,8 @@ const Footer = () => (
         <li>Dinero Coin Project. All rights reserved 2020 - 2021.</li>
         <li>
           <a href="https://github.com/dinerocoin/Dinero-Docs/blob/master/Dinero_Privacy_Policy.pdf">Privacy Policy</a>
+        </li>      
+        <li>
           <a href="https://www.cookieserve.com/">CookieServe Checker</a>
         </li>
       </ul>
