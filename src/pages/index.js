4553586babf1ef53e5aef467c97ebe0de3b41ba0
casterminator <44626745+casterminator@users.noreply.github.com>
2021-04-28 16:33:36 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const IndexPage = () => (
             <img src={Logo} alt="" data-position="25% 25%" />
           </a>
           <p>
-            Dependable Decentralized Global Transactions by Mykke
+            Dependable Decentralized Global Transactions
           <br />
           </p>
           <ul className="actions">
